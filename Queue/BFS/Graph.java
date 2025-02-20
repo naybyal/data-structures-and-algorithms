74fcc26b660f478528f12a7b8767f56c2a98768f
naybyal <officiallynabiel@gmail.com>
2025-02-21 00:28:21 +0530

    Queue: Breadth First Search

--- Queue/BFS/Graph.java
+++ Queue/BFS/Graph.java
@@ -43,9 +43,8 @@ class Graph {
     }
 
     public static void main(String[] args) {
-        Graph graph = new Graph(6); // Create a graph with 6 vertices
-
-        // Add edges to the graph
+        Graph graph = new Graph(6); 
+        
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
         graph.addEdge(1, 3);
