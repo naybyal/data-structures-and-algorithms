47969627b28b15403a5eeec6c9909ac32810c5aa
naybyal <officiallynabiel@gmail.com>
2025-02-20 22:09:06 +0530

    Trees: Preorder traversal

---
+++ Trees/Preorder Traversal/Main.java
@@ -0,0 +1,25 @@
+//  Pre-order traversal of a tree 
+import java.io.*;
+
+public class TreeNode {
+  //  Tree Data Structure 
+  int val;
+  TreeNode left, right;
+  TreeNode() {}
+  TreeNode(int val) {
+    this.val = val;
+  }
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+public class Main {
+
+
+  public static void main(String[] args) {
+    
+  }
+}
